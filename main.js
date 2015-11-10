a601bf2c26423784a7b2d014f2ee366cdd49e2e4
4545445 <jo6986@gmail.com>
2015-11-10 18:26:02 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+function setup() {
+  // Create the canvas
+  createCanvas(720, 400);
+
+  
+  // Set colors
+  fill(200, 101, 192, 127);
+  stroke(127, 63, 120);
+
+  // A rectangle
+  rect(40, 77, 300, 300);
+  //
+  
+  
+    // A rectangle
+  rect(66, 102.5, 250, 250);
+  //
+    
+    
+    // A rectangle
+  rect(82, 117, 220, 220);
+  //
+    
+         // A rectangle
+  rect(97, 127, 190, 200);
+  //
+    
+    // A rectangle
+  rect(112, 155, 160, 144);
+  //
+}
