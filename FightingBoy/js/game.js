90243e7fa05972a9cc819645bf8edabcb6d5236e
Jason Oliver <jasonc.oliver@mac32517.bmcc.cuny.edu>
2015-12-08 19:50:04 -0500

    moving the load sprite sheet and animation to preload

--- FightingBoy/js/game.js
+++ FightingBoy/js/game.js
@@ -16,18 +16,19 @@ var img;//testing image loading
 function preload() {
     // Load the json for the tiles sprite sheet
   settings = loadJSON('JSON/game.json');
+      player_sprite_sheet= loadSpriteSheet(settings.player_sprite.path, settings.player_sprite.widthfile, settings.player_sprite.height, settings.player_sprite.numberofframes);
+    console.log('spritepath',settings.player_sprite.path);
+    console.log('player_sprite_sheet',player_sprite_sheet);
+     //2 Player walk animation passing in a SpriteSheet
+  player_walk = loadAnimation(player_sprite_sheet);
+console.log('player_walk',player_walk);
 }//preload 
 
 function setup() {
   var cnv = createCanvas(800, 400);
      cnv.parent("walking");
     
-  player_sprite_sheet= loadSpriteSheet(settings.player_sprite.path, settings.player_sprite.widthfile, settings.player_sprite.height, settings.player_sprite.numberofframes);
-    console.log('spritepath',settings.player_sprite.path);
-    console.log('player_sprite_sheet',player_sprite_sheet);
-     //2 Player walk animation passing in a SpriteSheet
-  player_walk = loadAnimation(player_sprite_sheet);
-console.log('player_walk',player_walk);
+
     
   // Create the Player sprite and add it's animations
   player_sprite = createSprite(settings.player_start.x,settings.player_start.y,settings.player_start.width,settings.player_start.height);
